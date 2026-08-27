96803a23393119d90e044a77a05544ee39b6a326
Nicolas Gaete <nicolasgaete.dev@gmail.com>
2026-08-27 01:02:17 -0400

    feat: add PDF text extraction

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
+import pymupdf
+
+
+def extraer_texto_pdf(ruta_pdf):
+    pdf = pymupdf.open(ruta_pdf)
+
+    texto_completo = ""
+
+    for pagina in pdf:
+        texto_completo += pagina.get_text()
+
+    pdf.close()
+
+    return texto_completo
+
+
 def main():
-    print("PDF to Excel - proyecto iniciado correctamente")
+    ruta_pdf = "input/prueba.pdf"
+
+    texto = extraer_texto_pdf(ruta_pdf)
+
+    print(texto)
 
 
 if __name__ == "__main__":
